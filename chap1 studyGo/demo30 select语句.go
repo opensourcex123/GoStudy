78abb288352557fe1315ec8dabea42b073402dad
nfx_git <13561592791@163.com>
2021-11-12 21:14:18 +0800

    select语句

--- chap1 studyGo/demo30 select语句.go
+++ chap1 studyGo/demo30 select语句.go
@@ -12,9 +12,9 @@ func main() {
 	select {
 	case <-ch1:
 		fmt.Println("case1可执行。。。。")
-		case <-ch2:
-			fmt.Println("case2可执行...")
-			case <-time.After(3 * time.Second):	// 其他两个case因为没有数据在chan中，所以被阻塞
-				fmt.Println("case3执行，timeout。。")
+	case <-ch2:
+		fmt.Println("case2可执行...")
+	case <-time.After(3 * time.Second): // 其他两个case因为没有数据在chan中，所以被阻塞
+		fmt.Println("case3执行，timeout。。")
 	}
 }

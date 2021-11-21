34126a04065bb3faf2f8d9ded0468fecafb95cc9
nfx_git <13561592791@163.com>
2021-11-21 18:51:17 +0800

    字典树

---
+++ chap1 studyGo/demo35 字典树.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+)
+
+type TrieNode struct {
+	value      int
+	dictionary [26]*TrieNode
+}
+
+type TrieTree struct {
+	root *TrieNode
+}
+
+func main() {
+	tree := createTree()
+
+	flag := tree.findWord("her")
+	fmt.Println(flag)
+	flag = tree.findWord("x")
+	fmt.Println(flag)
+}
+
+func createTree() TrieTree {
+	arrList := []string{"how", "hi", "her", "hello", "so", "see"}
+	myTree := TrieTree{}
+
+	// 添加根节点
+	myTree.root = &TrieNode{}
+	for _, value := range arrList {
+		myTree.addWord(value)
+	}
+	return myTree
+}
+
+func (t *TrieTree) addWord(word string) {
+	fmt.Println(word)
+	nowNode := t.root
+	a := int('a')
+	var char int
+
+	for i := 0; i < len(word); i++ {
+		char = int(word[i])
+		if nowNode.dictionary[char-a] != nil {
+			nowNode = nowNode.dictionary[char-a]
+			continue
+		} else {
+			newNode := &TrieNode{}
+			nowNode.dictionary[char-a] = newNode
+			nowNode = newNode
+			continue
+		}
+	}
+}
+
+func (t *TrieTree) findWord(word string) int {
+	nowNode := t.root
+	a := int('a')
+	var char int
+	for i := 0; i < len(word); i++ {
+		char = int(word[i])
+		if nowNode.dictionary[char-a] != nil {
+			return 0
+		} else {
+			nowNode = nowNode.dictionary[char-a]
+		}
+		if i == len(word)-1 {
+			return 1
+		}
+	}
+	return 0
+}

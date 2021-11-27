1a7ef83c86cb334c278ed548536f51a1d90f90be
nfx_git <13561592791@163.com>
2021-11-27 21:14:44 +0800

    冒泡排序

---
+++ chap1 studyGo/demo41  冒泡排序.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{1, 9, 10, 30, 2, 5, 45, 8, 63, 234, 12}
+	fmt.Println(GetMax(arr))
+	fmt.Println(BubbleSort(arr))
+}
+
+func GetMax(arr []int) int {
+	for i := 1; i < len(arr); i++ {
+		if arr[i-1] > arr[i] {
+			arr[i-1], arr[i] = arr[i], arr[i-1]
+		}
+	}
+	return arr[len(arr)-1]
+}
+
+func BubbleSort(arr []int) []int {
+	for i := 0; i < len(arr); i++ {
+		for j := i + 1; j < len(arr); j++ {
+			if arr[i] > arr[j] {
+				arr[i], arr[j] = arr[j], arr[i]
+			}
+		}
+	}
+	return arr
+}
